98f42da71329d8b89e79e80a0cb41d1f6d3ce045
Adeel <adeel2@umbc.edu>
2009-07-11 13:31:21 -0400

    python packaging

---
+++ setup.py
@@ -0,0 +1,17 @@
+from ez_setup import use_setuptools
+use_setuptools()
+from setuptools import setup
+
+desc = open('README').read()
+
+setup(name='berry',
+      version='0.1',
+      description="a minimalist web framework",
+      long_description=desc,
+      url='http://adeel.github.com/berry',
+      author='Adeel Ahmad Khan',
+      author_email='adeel2@umbc.edu',
+      py_modules=['berry'],
+      include_package_data=True,
+      data_files=[('', ['README'])],
+)
