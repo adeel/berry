34dfbc8ecba973f034836f367c54946ba5c88d8b
Adeel <adeel2@umbc.edu>
2009-07-16 18:05:08 -0400

    (1) Improved error handling.  (2) Logging to stderr.  (3) Adding custom HTTP headers.  (4) Some other stuff.

--- berry.py
+++ berry.py
@@ -2,19 +2,15 @@
 Berry is a minimal web framework written in Python.
 
 import berry
-from berry import get, redirect
+from berry import get
 
 @get('^$')
 def index(req):
-  return "HOME"
+  return "<h1>Welcome to the home page.</h1>"
 
-@get('^(\d+)/?$')
-def test(req, id):
-  return str(id)
-
-@get('^google/?$')
-def google(req):
-  raise redirect('http://google.com')
+@get('^hello/(.+)/?$')
+def hello(req, name):
+  return "<h1>Hello, %s!</h1>"
 
 berry.start()
 """
@@ -22,10 +18,21 @@ berry.start()
 import sys
 import re
 import cgi
+import logging
+import traceback
 import paste.httpserver
+
+debug = False
 
 routes = []
 middlewares = []
+error_handlers = {}
+
+logger = logging.getLogger('berry')
+logger.setLevel(logging.DEBUG)
+handler = logging.StreamHandler()
+handler.setFormatter(logging.Formatter('  [%(asctime)s] %(message)s'))
+logger.addHandler(handler)
 
 def start(host='127.0.0.1', port=4567):
   "Start the application."
@@ -34,6 +41,7 @@ def start(host='127.0.0.1', port=4567):
     serve(host, port)
   except KeyboardInterrupt:
     sys.exit()
+  
 
 def use(middleware, options=None):
   "Use a middleware.  (Can take options.)"
@@ -56,73 +64,94 @@ def serve(host, port):
 def handle_request(env, start_response):
   "The WSGI application."
   
-  request = Request(env, start_response)
-  route, params = dispatch(request)
-  if not route:
-    return ErrorHandler(request, NotFound).error()
-  
-  try:
-    output = route.handler(request, *params)
-  except Exception, exception:
-    if isinstance(exception, HTTPError):
-      return ErrorHandler(request, exception).error()
-    else:
-      return ErrorHandler(request, AppError).error()
-  
-  print '200: "%s"' % request.path
+  request = Request(env, start_response)  
+  response = Response(request)
+  return response.get()
 
-  content_type = getattr(route.handler, 'content_type', 'text/html')
-  start_response('200 OK', [('Content-Type', content_type)])
+class Response(object):
   
-  return output
+  def __init__(self, request):
+    self.request = request
+  
+  def get(self):
+    route, urlparams = self.request._dispatch()
+    
+    if not route:
+      exception = NotFound()
+      status = exception.status
+      handler = exception.get_handler()
+    else:
+      status = (200, 'OK')
+      handler = route.handler
+      
+      try:
+        content = handler(self.request, *urlparams)
+      except Exception, exception:
+        if not isinstance(exception, HTTPError):
+          exception = AppError()
+        status = exception.status
+        handler = exception.get_handler()
+    
+    log(status, self.request)
+    
+    headers = getattr(handler, 'headers', {})
+    if 'Content-Type' not in headers:
+      headers['Content-Type'] = 'text/html'
+    
+    self.request._start_response('%s %s' % status, headers.items())
+    
+    if status[0] == 200:
+      return content
+    else:
+      return handler(self.request)
+  
 
 class HTTPError(Exception):
   "Base class for HTTP errors."
-
-class NotFound(HTTPError):
-  status = 404
-
-class AppError(HTTPError):
-  status = 500
+  
+  status = (500, 'Internal Server Error')
+  content = ""
+  headers = {}
+  
+  def get_handler(self):
+    code = self.status[0]
+    if code in error_handlers:
+      handler = error_handlers[code]
+    else:
+      def handler(req):
+        return self.content
+      handler.headers = self.headers
+    
+    return handler
+  
 
 class Redirect(HTTPError):
-  status = 303
+  status = (303, 'See Other')
   
   def __init__(self, url):
-    self.url = url
+    self.headers = {'Location': url}
   
 
-class ErrorHandler(object):
-  "Handle HTTP errors."
+class Forbidden(HTTPError):
+  status = (403, 'Forbidden')
+  content = "<h1>403 Forbidden</h1>"
+
+class NotFound(HTTPError):
+  status = (404, 'Not Found')
+  content = "<h1>404 Not Found</h1>"
+
+class AppError(HTTPError):
+  status = (500, 'Internal Server Error')
   
-  def __init__(self, request, exception):
-    self.request = request
-    self.exception = exception
-    self.status = getattr(self.exception, 'status', 404)
-    print '%s: %s' % (self.status, self.exception)
-  
-  def error(self):
-    name = 'error_' + str(self.status)
-    try:
-      handler = self.__getattribute__(name)
-    except:
-      handler = self.error_404
-    return handler()
-  
-  def error_404(self):
-    self.request._start_response('404 Not Found',
-      [('Content-type', 'text/plain')])
-    return 'Not Found'
-  
-  def error_500(self):
-    self.request._start_response('500 Internal Server Error',
-      [('Content-type', 'text/plain')])
-    return 'Internal Server Error'
-  
-  def error_303(self):
-    self.request._start_response('303 See Other',
-      [('Content-type', 'text/plain'), ('Location', self.exception.url)])
-    return ''
+  def __init__(self):
+    content = "<h1>500 Internal Server Error</h1>"
+    
+    if debug:
+      exc_info = sys.exc_info()
+      tb = ''.join(traceback.format_exception(*exc_info))
+      content += "<pre><code>%s</code></pre>" % tb
+    
+    self.content = content
   
 
 class Request(object):
@@ -133,9 +162,22 @@ class Request(object):
     self._start_response = start_response
     
     self.path = self.env.get('PATH_INFO', '').lstrip('/')
-    self.method = self.env.get('REQUEST_METHOD', 'GET').upper()
     self.query = self.env.get('QUERY_STRING', '')
+    self.fullpath = '/' + self.path
+    if self.query:
+      self.fullpath += '?' + self.query
     self.params = self._parse_params()
+    self.method = self.env.get('REQUEST_METHOD', 'GET').upper()
+  
+  def _dispatch(self):
+    "Dispatch the request."
+    
+    for route in routes:
+      if route.method == self.method:
+        match = re.search(route.path, self.path)
+        if match is not None:
+          return route, list(match.groups())
+    return None, []
   
   def _parse_params(self):
     "Parse all form data."
@@ -178,15 +220,23 @@ class Request(object):
     return params
   
 
-def dispatch(request):
-  "Dispatch the request."
+def error(code):
+  "A decorator for registering error handlers."
   
-  for route in routes:
-    if route.method == request.method:
-      match = re.search(route.path, request.path)
-      if match is not None:
-        return route, list(match.groups())
-  return None, []
+  def register(handler):
+    error_handlers[code] = handler
+    return handler
+  return register
+
+def header(key, value):
+  "A decorator for adding headers."
+  
+  def register(handler):
+    if not getattr(handler, 'headers', {}):
+      handler.headers = {}
+    handler.headers[key.title()] = value
+    return handler
+  return register
 
 def get(path):
   "The decorator for GET."
@@ -211,9 +261,19 @@ def post(path):
 class Route(object):
   "Abstraction of routes."
   
-  def __init__(self, path, handler=None, method='GET'):
+  def __init__(self, path, handler, method='GET'):
     path = path.lstrip('/')
     self.path = path
     self.handler = handler
     self.method = method.upper()
   
+
+def log(status, request):
+  message = '%s: %s' % (status, request.fullpath)
+  
+  if status[0] == 200:
+    logger.info(message)
+  elif status[0] == 500:
+    logger.exception(message)
+  else:
+    logger.error(message)
