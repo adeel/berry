ca3d338923455d1038c7fe36f3260dfad9d6e6db
Adeel Ahmad Khan <adeel2@umbc.edu>
2009-08-20 15:11:14 -0400

    tests/run.py: edited output format

--- tests/run.py
+++ tests/run.py
@@ -1,6 +1,12 @@
 from cherrypy.test.webtest import *
 
+tests = ('request', 'forms')
+
 if __name__ == '__main__':
-  suite = ReloadingTestLoader().loadTestsFromName('request_test.RequestTest')
-  suite = ReloadingTestLoader().loadTestsFromName('forms_test.FormsTest')
-  TextTestRunner().run(suite)
+  for test in tests:
+    name = '%s_test.%sTest' % (test, test.title())
+    suite = ReloadingTestLoader().loadTestsFromName(name)
+    
+    print 'Running tests in %s...' % name
+    TextTestRunner(verbosity=0).run(suite)
+    print ''
